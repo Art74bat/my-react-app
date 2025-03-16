b4f41aaf01f25892310c40db977b5155a4f4babc
Art74bat <123627505+Art74bat@users.noreply.github.com>
2025-03-17 00:27:45 +0300

    Update

--- src/Pages/components/admin/AdminPrice.jsx
+++ src/Pages/components/admin/AdminPrice.jsx
@@ -125,7 +125,7 @@ export default function AdminPrice () {
 
                 {/* Строки с данными */}
                 {items[1].map((item) => (
-                    <tr key={item.id}>
+                    <tr key={item.id} className={style.form_wrapp}>
                     <td><span className={style.table_title}>Описание: </span>{item.description}</td>
                     <td><span className={style.table_title}>Устройство: </span>{item.title}</td>
                     <td><span className={style.table_title}>Модель: </span>{item.sub_title}</td>
@@ -141,67 +141,59 @@ export default function AdminPrice () {
                 ))}
 
                 {/* Форма для добавления новой записи */}
-                <tr>
+                <tr className={style.form_wrapp}>
+                    <td className={style.form}>
+                    <input
+                        className={style.form_input}
+                        type="text"
+                        name="category"
+                        placeholder="Категория*"
+                        onChange={handleChange}
+                    />
+                    </td>
+                    <td className={style.form}>
+                        <input
+                            className={style.form_input}
+                            type="text"
+                            name="title"
+                            placeholder="Устройство*"
+                            onChange={handleChange}
+                        />
+                    </td>
+                    <td className={style.form}>
+                        <input
+                            className={style.form_input}
+                            type="text"
+                            name="sub_title"
+                            placeholder="Модель*"
+                            onChange={handleChange}
+                        />
+                    </td>
+                    <td className={style.form}>
+                        <input
+                            className={style.form_input}
+                            type="text"
+                            name="description"
+                            placeholder="Описание*"
+                            onChange={handleChange}
+                        />
+                    </td>
+                    <td className={style.form}>
+                        <input
+                            className={style.form_input}
+                            type="text"
+                            name="price"
+                            placeholder="Цена*"
+                            onChange={handleChange}
+                        />
+                    </td>
                     <td>
-                        <table className="inner-table">
-                            <tbody>
-                            <tr>
-                                <td className={style.form}>
-                                <input
-                                    className={style.form_input}
-                                    type="text"
-                                    name="category"
-                                    placeholder="Категория*"
-                                    onChange={handleChange}
-                                />
-                                </td>
-                                <td className={style.form}>
-                                <input
-                                    className={style.form_input}
-                                    type="text"
-                                    name="title"
-                                    placeholder="Устройство*"
-                                    onChange={handleChange}
-                                />
-                                </td>
-                                <td className={style.form}>
-                                <input
-                                    className={style.form_input}
-                                    type="text"
-                                    name="sub_title"
-                                    placeholder="Модель*"
-                                    onChange={handleChange}
-                                />
-                                </td>
-                                <td className={style.form}>
-                                <input
-                                    className={style.form_input}
-                                    type="text"
-                                    name="description"
-                                    placeholder="Описание*"
-                                    onChange={handleChange}
-                                />
-                                </td>
-                                <td className={style.form}>
-                                <input
-                                    className={style.form_input}
-                                    type="text"
-                                    name="price"
-                                    placeholder="Цена*"
-                                    onChange={handleChange}
-                                />
-                                </td>
-                                <td>
-                                <button
-                                    className={style.btn}
-                                    onClick={() => createItem(items[1], token)}
-                                >
-                                    Добавить
-                                </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                        </table>
+                        <button
+                            className={style.btn}
+                            onClick={() => createItem(items[1], token)}
+                        >
+                            Добавить
+                        </button>
                     </td>
                 </tr>
                 </React.Fragment>
@@ -321,11 +313,12 @@ export default function AdminPrice () {
 
     return(
         <>
-        <h2>Прайс лист</h2>
+        <h2 className={style.title}>Прайс лист</h2>
         <table className={style.table}>
             <thead>
-                <tr colSpan="3">
+                <tr>
                     <th>Аппаратная часть</th>
+                    <th></th>
                     <th></th>
                     <th></th>
                     <th></th>
@@ -340,6 +333,8 @@ export default function AdminPrice () {
                     <th>Программная часть</th>
                     <th></th>
                     <th></th>
+                    <th></th>
+                    <th></th>
                 </tr>
             </thead>
             {outSoft}
@@ -348,6 +343,8 @@ export default function AdminPrice () {
         <thead>
                 <tr colSpan="3">
                     <th>Для Юридических лиц</th>
+                    <th></th>
+                    <th></th>
                     <th></th>
                     <th></th>
                 </tr>
